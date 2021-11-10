aa85ee9cd1e30b309c918df123ffe4710faa21d9
Eddy <eddy.gonzalez.chavez.al@iespoligonosur.org>
2021-11-10 12:03:05 +0100

    completado

---
+++ ejercicio5.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<?php
+    include "ejercicio5BD.php";
+?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link rel="stylesheet" href="ejercicio5.css">
+        <title>Registro</title>
+    </head>
+    <body>
+        <?php
+            $nombreUsuario = "";
+            $contrasenya = "";
+            $cuenta_bank = "";
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                $nombreUsuario = $_POST["nombreUsuario"];
+                $contrasenya = $_POST["contrasenya"];
+                $cuenta_bank = $_POST["cuenta_bank"];
+
+                //encriptada
+                $contrasenya = password_hash($contrasenya,PASSWORD_DEFAULT);
+
+                insertaElemento($nombreUsuario,$contrasenya,$cuenta_bank);
+            }
+
+        ?>
+
+        <form class="form-register" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST" enctype="multipart/form-data">
+            <h2 class="form-titulo">Registro al Banco</h2>
+            <div class="contenedor-inputs">
+                <input type="text" name="nombreUsuario" placeholder="Nombre Usuario" class="input-100" required>
+                <input type="password" name="contrasenya" placeholder="Password" class="input-100" required>
+                <input type="text" name="cuenta_bank" placeholder="Cuenta Bancaria" class="input-100" required>                
+                <input type="submit" value="Registrar" class="btn-enviar" name="submit">
+                <div id="errores"></div>
+            </div>
+        </form>
+    </body>
+</html>
---
+++ ejercicio5BD.php
@@ -0,0 +1,24 @@
+<?php
+    $servidor = "localhost";
+    $baseDatos = "usuarios";
+    $usuario = "developer";
+    $pass = "developer";
+    function insertaElemento($nombreUsuario,$contrasenya,$cuentaBank){
+            try {
+                $conexion = new PDO("mysql:host=$GLOBALS[servidor];dbname=$GLOBALS[baseDatos]", $GLOBALS["usuario"], $GLOBALS["pass"]);
+            
+                $sql = $conexion->prepare("INSERT INTO users (nombreUsuario, contrasenya, cuenta_bank) VALUES (:nombreUsuario,:contrasenya,:cuenta_bank)");
+                
+                $sql->bindParam(":nombreUsuario", $nombreUsuario);
+                $sql->bindParam(":contrasenya", $contrasenya);
+                $sql->bindParam(":cuenta_bank", $cuentaBank);;
+    
+                $sql->execute();
+                $last_id = $conexion->lastInsertId(); //return no me devuelve el id
+                return $last_id;
+            } catch (PDOException $e) {
+                return $e->getMessage();
+            }
+            $conexion=null;
+    }
+?>
